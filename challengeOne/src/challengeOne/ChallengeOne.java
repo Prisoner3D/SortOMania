0ab43e3dc2afc38d324b83a963b5a259503decdc
Thomas <Thomas@DESKTOP-52CN92E>
2017-12-07 01:24:21 -0500

    broke

--- challengeOne/src/challengeOne/ChallengeOne.java
+++ challengeOne/src/challengeOne/ChallengeOne.java
@@ -9,31 +9,67 @@ public class ChallengeOne {
 		//Add removing two lowest times
 		//Add median
 		long time = 0;
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < 1; i++)
 		{
-			int[] challengeOne = new int[10000];
+			int[] challengeOne = new int[100];
 			for (int j = 0; j < challengeOne.length; j++)
 			{
-				challengeOne[j] = (int) (Math.random() * 10001);
+				challengeOne[j] = (int) (Math.random() * 100);
 			}
 			
 			
 			long startTime = System.nanoTime();
 					
 			//Sorts.Bsort(challengeOne, 10000);
-			challengeOne = Sorts.countingSort(challengeOne, 10000); 
+			challengeOne(challengeOne);
 			
 			long endTime = System.nanoTime(); //record stopTime
 			long totalTime = endTime - startTime; //calculate totalTime
 			System.out.println("Time Taken in milliseconds: " + totalTime / 1000000.0000000);
-
+			System.out.println(challengeOne.length);
 			for (int num : challengeOne)
 			{
-				//System.out.println(num);
+				System.out.println(num);
 			}
 			
 			time += totalTime;
 		}
-		System.out.println(time / 10);
+		System.out.println(time / 1);
+	}
+	
+	public static int challengeOne(int[] dataSet)
+	{
+		int[] copy = dataSet;
+		int[] count = new int[dataSet.length];
+		for (int i = 0; i < dataSet.length; i++)
+		{
+			count[copy[i]]++;
+		}
+		for (int i = 1; i < count.length; i++)
+		{
+			count[i] = count[i] + count[i-1];
+		}
+		int[] sorted = new int[dataSet.length];
+		for (int i = 0; i < dataSet.length; i++)
+		{
+			sorted[count[copy[i]]] = copy[i];
+			count[copy[i]]--;
+		}
+
+		for (int i = 0; i < dataSet.length; i++)
+		{
+			dataSet[i] = sorted[i];
+		}
+		return median(dataSet); //Shorten maybe
+
+	}
+	
+	public static int median(int[] list1)
+	{
+		if(list1.length % 2 == 1)
+		{
+			return (list1[(int)(list1.length/2) + 1]);
+		}
+		return ((list1[list1.length/2])+(list1[(list1.length/2)-1]))/2;
 	}
 }
